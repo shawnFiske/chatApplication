b021218e9aecd6b8616d6804b5fdb4b6d6135efa
Shawn Fiske <durah_shar@hotmail.com>
2015-12-19 14:17:15 -0800

    added cleint updates

--- app/client.js
+++ app/client.js
@@ -1,5 +1,35 @@
-(function (val) {
-	// ... all vars and functions are in this scope only
-	// still maintains access to all globals
-	//test
-}(val));
+var app = angular.module('sampleApp', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/', {
+    templateUrl: '/index.html'
+  });
+}]);
+
+app.factory('socket', ['$rootScope', function($rootScope) {
+  var socket = io.connect();
+
+  return {
+    on: function(eventName, callback){
+      socket.on(eventName, callback);
+    },
+    emit: function(eventName, data) {
+      socket.emit(eventName, data);
+    }
+  };
+}]);
+
+app.controller('IndexController', function($scope, socket) {
+  $scope.newCustomers = [];
+  $scope.currentCustomer = {};
+
+  $scope.join = function() {
+    socket.emit('add-customer', $scope.currentCustomer);
+  };
+
+  socket.on('notification', function(data) {
+    $scope.$apply(function () {
+      $scope.newCustomers.push(data.customer);
+    });
+  });
+});
